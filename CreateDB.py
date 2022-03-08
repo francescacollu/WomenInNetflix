8e669c8f165ae9a79c448dac1c4f1d979e629f2e
Francesca Collu <colluf6@gmail.com>
2022-03-08 10:31:13 -0800

    Added test directory

--- CreateDB.py
+++ CreateDB.py
@@ -1,4 +1,8 @@
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
 
+movies_df = pd.read_csv('data/netflix_titles.csv')
+names_df = pd.read_csv('data/wgnd_langctry.csv')
+country_df = pd.read_csv('data/country_list.csv')
